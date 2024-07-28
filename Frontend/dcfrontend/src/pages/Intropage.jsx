8ed9067ad2a3df3b28f8317c93a8092bfc68f078
YujenM <maharjanyuzen@gmail.com>
2024-07-28 14:42:44 +0545

    Fontaweosome icon added

--- Frontend/dcfrontend/src/pages/Intropage.jsx
+++ Frontend/dcfrontend/src/pages/Intropage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as icon from '@fortawesome/free-solid-svg-icons';
+
 function Intropage() {
   return (
     <div>
         <h1>Intro Page</h1>
+        <FontAwesomeIcon icon={icon.faBook}/>
         <Link type='button' to='/Login'>Login</Link>
         <Link to='/Signup'>Signup</Link>
     </div>
