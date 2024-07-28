966f2096888fe677013aaa89766a38237d2e2240
YujenM <maharjanyuzen@gmail.com>
2024-07-28 11:15:37 +0545

    Routes added and pages added

--- Frontend/dcfrontend/src/App.js
+++ Frontend/dcfrontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+
+  BrowserRouter as Router,
+  
+  Routes,
+  
+  Route,
+  
+  } from 'react-router-dom';
+import Intropage from './pages/Intropage';
+import Login from './pages/Login';
+import Signuppage from './pages/Signuppage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Intropage/>}/>
+          <Route exact path='/Login' element={<Login/>}/>
+          <Route exact path='/Signup' element={<Signuppage/>}/>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
