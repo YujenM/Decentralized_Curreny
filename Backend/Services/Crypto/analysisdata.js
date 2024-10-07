3648ad9aba97a1e93c2d62689e33762680979b01
YujenM <maharjanyuzen@gmail.com>
2024-10-07 21:06:18 +0545

    routes added

---
+++ Backend/Services/Crypto/analysisdata.js
@@ -0,0 +1,19 @@
+const db=require('../../Database/ConnectDb');
+
+const fectchanalysisdata=async(req,res)=>{
+    try{
+        const analysisdataquerry='SELECT cc.UUID, cc.Crypto_Symbol, cc.Crypto_image FROM Crypto_Currencies cc';
+        const analysisresult= await db.getquery(analysisdataquerry);
+        // console.log(analysisresult);
+        res.send(analysisresult);
+        console.log('Data fetched successfully');
+
+    }catch(err){
+        throw new Error(`Error fetching analysis data:${err.message}`);
+    }
+
+}
+
+module.exports={
+    fectchanalysisdata
+}
